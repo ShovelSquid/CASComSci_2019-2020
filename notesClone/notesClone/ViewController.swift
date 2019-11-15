b94a94412a1d506c10f97cc33f5f4c635545da57
ShovelSquid <kaelen1cook@gmail.com>
2019-11-14 18:04:03 -0800

    started notes clone

---
+++ notesClone/notesClone/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  notesClone
+//
+//  Created by Kaelen Cook on 11/14/19.
+//  Copyright © 2019 Kaelen Cook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var textField: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
