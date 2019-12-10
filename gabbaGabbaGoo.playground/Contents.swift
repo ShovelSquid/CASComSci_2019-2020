221e80fe4372298568a9642e14790b1cd77cc274
ShovelSquid <kaelen1cook@gmail.com>
2019-12-10 09:07:33 -0800

    gabbaGabbaGoo

---
+++ gabbaGabbaGoo.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+let xrds: [String: Any] = [
+    "name" : "Crossroads School for the Arts and Sciences",
+    "foundingYear": 1971,
+    "campuses": [
+        "Norton",
+        "21st Street"
+    ]
+]
+
+
+let name0 = xrds["name"]
+let name1 = xrds["name"] as! String
+let name2 = xrds["name"] as? String
+let urmum = "gay"
+
+print(name1)
