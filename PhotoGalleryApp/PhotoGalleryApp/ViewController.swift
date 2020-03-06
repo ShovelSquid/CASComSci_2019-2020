a956f57946d7a80ac36e901f22633d25da76dfa7
ShovelSquid <kaelen1cook@gmail.com>
2020-03-06 08:37:01 -0800

    finished filter app
    starting gallery app

---
+++ PhotoGalleryApp/PhotoGalleryApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoGalleryApp
+//
+//  Created by Kaelen Cook on 3/6/20.
+//  Copyright © 2020 Kaelen Cook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- aBasicSwapiApp/PhotoFilters/PhotoFilters/ViewController.swift
+++ aBasicSwapiApp/PhotoFilters/PhotoFilters/ViewController.swift
@@ -8,21 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var pickerView: UIPickerView!
     @IBOutlet weak var barButton: UIBarButtonItem!
+    @IBOutlet weak var selectButton: UIButton!
     
     var image: UIImage?
+    
+    var filters = ["Chrome", "Noir", "Process", "Tonal", "Instant", ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        selectButton.addTarget(self, action: #selector(selectFilter), for: .touchUpInside)
+        pickerView.delegate = self
+        pickerView.dataSource = self
+    }
+    
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return filters[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return filters.count
+    }
+    
+    
+    
+    @objc func selectFilter() {
+        let inputImage = image!
+        let row = pickerView.selectedRow(inComponent: 0)
+        let selectedFilter = "CIPhotoEffect\(filters[row])"
+        
+        if let filter = CIFilter(name: selectedFilter) {
+            let beginImage = CIImage(image: inputImage)
+            filter.setDefaults()
+            filter.setValue(beginImage, forKey: kCIInputImageKey)
+            
+            if let output = filter.outputImage {
+                let processedImage = UIImage(ciImage: output)
+                imageView.image = processedImage
+            }
+        }
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         self.dismiss(animated: true, completion: nil)
         image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        
+        imageView.image = image
     }
 
     @IBAction func launchPhotoPicker(_ sender: Any) {
